68eec74a589d0e5031ce677671aa349c5ec3b69a
elmarolafss <elmarol250@nemi.tskoli.is>
2017-01-25 14:04:05 +0000

    aðeins lengra kominn

--- Git-gsö-py.py
+++ Git-gsö-py.py
@@ -4,4 +4,6 @@ w.close()
 w = open("Textraskra.txt", "w")
 w.write("Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,")
 w.write("when an unknown printer took a galley of type and scrambled it to make a type specimen book.")
+w.write("It has survived not only five centuries.")
+w = open("Textaskra.txt", "r")
 
